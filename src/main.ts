df56afaab1e78b41f555ae984ddf16c869c39ef5
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-09-08 20:07:27 +0400

    Config and Utils Resource

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { port } from './utils/constants/port.constant';
+import { swagger } from './configs/swagger-configs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  swagger(app);
+
+  await app.listen(port);
 }
 bootstrap();
