b70ce03ce191afdb94f00c788a83b35e60c51306
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-09-08 16:44:10 +0400

    main project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, ProductModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
